9f16f91a28fb89cc959281bb8963a085ca509233
rbartosinski <r.bartosinski@wp.pl>
2021-11-16 17:05:09 +0100

    rb-corrections

--- store/forms.py
+++ store/forms.py
@@ -1,7 +1,5 @@
 from django import forms
 from django.forms import ModelForm
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
 from .models import Order
 
 
@@ -9,12 +7,6 @@ class OrderForm(ModelForm):
     class Meta:
         model = Order
         fields = '__all__'
-
-
-# class CreateUserForm(UserCreationForm):
-#     class Meta:
-#         model=User
-#         fields = ['username', 'email', 'password']
 
 
 class RegisterForm(forms.Form):
